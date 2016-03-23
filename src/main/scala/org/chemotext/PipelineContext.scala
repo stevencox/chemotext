f3d911f84c36c032f4aa74b91e52e7e396befe8a
Steven Cox <scox@renci.org>
2016-03-23 12:02:48 -0400

    CTD AB/BC/AC

--- src/main/scala/org/chemotext/PipelineContext.scala
+++ src/main/scala/org/chemotext/PipelineContext.scala
@@ -39,7 +39,7 @@ object Processor {
 
   val logger = LoggerFactory.getLogger("Processor")
 
-  type WordPair = (String, String, Double, Double, Double)
+  type WordPair = (String, String, Double, Double, Double, Int)
   type WordTriple = (String, String, String)
   type WordPosition = (String, Int, Int, Int)
 
@@ -112,16 +112,22 @@ object Processor {
   def findPairs (words : List[List[WordPosition]]) : List[List[WordPair]] = {
     val threshold = 100
     List (
-      findCooccurring (words (0), words (1), threshold), // AB
-      findCooccurring (words (1), words (2), threshold), // BC
-      findCooccurring (words (0), words (2), threshold)  // AC
+      findCooccurring (words (0), words (1), threshold, 1), // AB
+      findCooccurring (words (1), words (2), threshold, 2), // BC
+      findCooccurring (words (0), words (2), threshold, 3)  // AC
     )
   }
 
   /**
     * Determine pairs based on distance and include an confidence score 
     */
-  def findCooccurring (L : List[WordPosition], R : List[WordPosition], threshold : Int) : List[WordPair] = {
+  def findCooccurring (
+    L : List[WordPosition],
+    R : List[WordPosition],
+    threshold : Int,
+    code      : Int)
+      : List[WordPair] = 
+  {
     L.flatMap { left =>
       R.map { right =>
         val docPosDifference = math.abs (left._2 - right._2).toDouble
@@ -130,10 +136,11 @@ object Processor {
             right._1,
             docPosDifference,
             math.abs (left._3 - right._3).toDouble,
-            math.abs (left._4 - right._4).toDouble
+            math.abs (left._4 - right._4).toDouble,
+            code
           )
         } else {
-          ( null, null, 0.0, 0.0, 0.0 )
+          ( null, null, 0.0, 0.0, 0.0, 0 )
         }
       }
     }.filter { element =>
@@ -194,13 +201,15 @@ object Processor {
     if (words != null && text != null) {
       text.foreach { sentence =>
         words.foreach { word =>
-          val index = sentence.indexOf (word)
-          if (index > -1) {
-            sentence.split (" ").foreach { token =>
-              if (word.equals (token)) {
-                val tuple = ( word, docPos + textPos + index, paraPos, sentPos + sentenceIndex )
-                result.add ( tuple )
-                logger.debug (s"Adding result $tuple")
+          if (!word.equals ("for") && !word.equals ("was")) { // raw she ran cough
+            val index = sentence.indexOf (word)
+            if (index > -1) {
+              sentence.split (" ").foreach { token =>
+                if (word.equals (token)) {
+                  val tuple = ( word, docPos + textPos + index, paraPos, sentPos + sentenceIndex )
+                    result.add ( tuple )
+                  logger.debug (s"Adding result $tuple")
+                }
               }
             }
           }
@@ -212,164 +221,24 @@ object Processor {
     result.toList
   }
 
+
+  val ordinary=(('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9')).toSet
+  def isOrdinary(s:String) = s.forall (ordinary.contains (_))
+
   /**
     * Prep vocabulary terms by lower casing, removing non alpha strings and other dross.
     */
   def getDistinctAlpha (terms : RDD[String]) : List[String] = {
     terms.filter { text =>
       val lower = text.replaceAll("-", "")
-      ! (lower forall Character.isDigit) && lower.length > 1
+//      ! (lower forall Character.isDigit) && lower.length > 2
+      (! ( lower forall Character.isDigit) ) && 
+//      isOrdinary (lower) &&
+      lower.length > 2 &&
+      ( ( ! lower.equals ("was")) && ( ! lower.equals ("for")) )
     }.map { text =>
       text.toLowerCase ()
     }.distinct().collect ().toList
-  }
-
-  def executeChemotextPipeline (
-    articlePaths : RDD[String],
-    meshXML      : String,
-    ctdAC        : RDD[Array[String]],
-    ctdAB        : RDD[Array[String]],
-    ctdBC        : RDD[Array[String]],
-    sampleSize   : Double) =
-  {
-
-    logger.info ("Checking vocabulary...")
-    val vocab = VocabFactory.getVocabulary (meshXML)
-    if (! vocab.extended) {
-      logger.info ("Extending basic (MeSH) vocabulary with terms from CTD...")
-      // AB -> _1, _4
-      // BC -> _1, _3
-      // AC -> _1, _4
-      val AB = ctdAB.map { row => ( row(0), row(3) ) }
-      val BC = ctdBC.map { row => ( row(0), row(2) ) }
-      val AC = ctdAC.map { row => ( row(0), row(3) ) }
-      VocabFactory.writeJSON (new Vocabulary (
-        List.concat (vocab.A, getDistinctAlpha (AB.map { e => e._1 })),
-        vocab.B :::
-          getDistinctAlpha (AB.map { e => e._2 }) :::
-          getDistinctAlpha (BC.map { e => e._1 }),
-        List.concat (vocab.C, getDistinctAlpha (AC.map { e => e._2 })),
-        true),
-        "vocab_new.json")
-    }
-
-    logger.info ("== Analyze articles; calculate binaries.")
-    val binaries = articlePaths.map { a =>
-      ( a, s"$meshXML" )
-    }.sample (false, sampleSize).map { article =>
-
-      Processor.quantifyArticle (article._1, article._2)
-    }.map { item =>
-      Processor.findPairs (item)
-
-/*      Processor.findPairs (article._1, article._2)
- */
-    }.cache ()
-
-    logger.info ("== Calculate triples.")
-    val triples = binaries.flatMap { item =>
-      Processor.findTriples (item)
-    }.map { triple =>
-      ( triple, 1 )
-    }.reduceByKey (_ + _).map { tuple =>
-      ( tuple._1._1, tuple._1._2, tuple._1._3, tuple._2 )
-    }.distinct().sortBy (elem => -elem._4).cache ()
-
-    triples.foreach { a =>
-      logger.info (s"triple :-- ${a}")
-    }
-
-    
-    logger.info ("== Get known triples from CTD.")
-    val knownTriples = getKnownTriples (triples.collect (), ctdAC).collect ()
-
-    knownTriples.foreach { a =>
-      logger.info (s"known-triple :-- ${a}")
-    }
-
-    logger.info ("== Label known binaries based on known triples...")
-    logger.info ("== Calculate logistic regression model.")
-    val flatBinaries = binaries.flatMap { item =>
-      item
-    }.flatMap { item =>
-      item
-    }.cache ()
-
-    logger.info ("== Calculate ab binaries")
-    val abBinaries = flatBinaries.flatMap { item =>
-      // Find known A->B binaries and associate their features.
-      knownTriples.filter { known =>
-        known._1.equals (item._1) && known._2.equals (item._2)
-      }.map { k =>
-        ( k._1, k._2, item._3, item._4, item._5 )
-      }
-    }.distinct().cache ()
-
-    logger.info ("== Calculate bc binaries")
-    val bcBinaries = flatBinaries.flatMap { item =>
-      // Find known B->C binaries and associate their features.
-      knownTriples.filter { known =>
-        known._2.equals (item._1) && known._3.equals (item._2)
-      }.map { k =>
-        ( k._2, k._3, item._3, item._4, item._5 )
-      }
-    }.distinct().cache ()
-
-    logger.info ("== Calculating log regression model.")
-
-
-    val abUnionBcBinaries = abBinaries.union (bcBinaries)
-
-    logger.info (s" AB/union/BC -> ${abUnionBcBinaries.count ()}")
-    val LogRM = new LogisticRegressionWithLBFGS().run(
-      //abBinaries.union (bcBinaries).map { item =>
-      abUnionBcBinaries.map { item =>
-        new LabeledPoint (
-          label    = 1,
-          features = new DenseVector (Array( item._3, item._4, item._5 ))
-        )
-      })
-
-    logger.info ("== Use LogRM to associate probabilities with each binary.")
-    val abProb = abBinaries.map { binary =>
-      ( binary._1, binary._2,
-        LogRM.predict (new DenseVector (Array ( binary._3, binary._4, binary._5 ))) )
-    }.distinct().cache ()
-    abProb.collect().foreach { item =>
-      logger.info (s"AB with prob: $item")
-    }
-
-    val bcProb = bcBinaries.map { binary =>
-      ( binary._1, binary._2,
-        LogRM.predict (new DenseVector (Array ( binary._3, binary._4, binary._5 ))) )
-    }.distinct().cache ()
-    bcProb.collect ().foreach { item =>
-      logger.info (s"BC with prob: $item")
-    }
-
-    logger.info ("== Hypotheses:")
-    val join = abProb.map { i =>
-      (i._2, (i._1, i._3, 1 ))
-    }.cogroup (
-      bcProb.map { j =>
-        (j._1, (j._2, j._3, 2 ))
-      }
-    ).cache ()
-
-    join.collect().foreach { t =>
-      logger.info (s"join > $t")
-    }
-
-    join.flatMap { m =>
-      m._2._1.flatMap { n =>
-        m._2._2.map { p =>
-          (n._1, m._1, p._1, n._2 * p._2 )
-        }
-      }
-    }.cache ().foreach { triple =>
-      logger.info (s"hypothesis => $triple")
-    }
-
   }
 
   /**
@@ -400,6 +269,306 @@ object Processor {
         elem.trim.replaceAll ("\"", "")
       }
     }
+  }
+
+  /*
+   * The approach below finds pairs only if they occur within an article.
+   * More cohesive, less hits, etc.
+   */
+  def generatePairs (articlePaths : RDD[String], meshXML : String, sampleSize : Double) = {
+    logger.info ("== Analyze articles; calculate binaries.")
+    articlePaths.map { a =>
+      ( a, meshXML )
+    }.sample (false, sampleSize, 1234).map { article =>
+      findPairs (article._1, article._2)
+    }.cache ()
+  }
+
+  def formKey (a : String, b : String) : String = {
+    s"$a-$b"
+  }
+
+  def splitKey (k : String) : Array[String] = {
+    k.split ("-")
+  }
+
+  def joinBinaries (
+    L : RDD[(String, String, Int)],
+    R : RDD[(String, (Double, Double, Double, Int))]) :
+      RDD[WordPair] =
+  {
+    logger.info ("Joining binaries")
+    val left = L.map { item =>
+      ( formKey (item._1, item._2), (0) )
+    }
+    R.join (left).map { item =>
+      val k = splitKey (item._1)
+      ( k(0), k(1), item._2._1._1, item._2._1._2, item._2._1._3, item._2._1._4 )
+    }.distinct().cache ()
+  }
+
+  def trainLRM (samples : RDD[WordPair]) = {
+    logger.info (s"Training LRM with ${samples.count} binaries")
+    new LogisticRegressionWithLBFGS().run(
+      samples.sample (false, 0.01, 1234).map { item =>
+        new LabeledPoint (
+          label    = 1,
+          features = new DenseVector (Array( item._3, item._4, item._5 ))
+        )
+      })
+  }
+
+/*
+  def trainLRM (samples : RDD[(String, String, Double, Double, Double)], name : String) = {
+    var model : LogisticRegressionModel = null
+    if (Files.exists(Paths.get(name))) {
+      model = LogisticRegressionModel.load(sc, name)
+    } else {
+      logger.info (s"Training LRM with ${samples.count} binaries")
+      model = new LogisticRegressionWithLBFGS().run(
+        samples.map { item =>
+          new LabeledPoint (
+            label    = 1,
+            features = new DenseVector (Array( item._3, item._4, item._5 ))
+          )
+        })
+      model.save (name)
+    }
+    model
+  }
+ */
+
+  def modelProbabilities (
+    binaries: RDD[WordPair],
+    lrm : LogisticRegressionModel) =
+  {
+    logger.info ("== Use LogRM to associate probabilities with each binary.")
+    val prob = binaries.map { binary =>
+      ( binary._1, binary._2,
+        lrm.predict (new DenseVector (Array ( binary._3, binary._4, binary._5 ))) )
+    }.distinct().cache ()
+    prob.collect().foreach { item =>
+      logger.info (s"binaries with prob: $item")
+    }
+    prob
+  }
+
+  def extendVocabulary (
+    AB      : RDD[(String, String, Int)],
+    BC      : RDD[(String, String, Int)],
+    AC      : RDD[(String, String, Int)],
+    meshXML : String
+  ) = {
+
+    logger.info ("Checking vocabulary...")
+    val vocab = VocabFactory.getVocabulary (meshXML)
+    if (! vocab.extended) {
+      /*
+      logger.info ("Extending basic (MeSH) vocabulary with terms from CTD...")
+      // AB -> _1, _4
+      // BC -> _1, _3
+      // AC -> _1, _4
+
+      val A = List.concat (vocab.A, getDistinctAlpha (AB.map { e => e._1 })).to[ListBuffer]
+
+      val B = vocab.B :::
+      getDistinctAlpha (AB.map { e => e._2 }) :::
+      getDistinctAlpha (BC.map { e => e._1 }).filter { lower =>
+        (! ( lower forall Character.isDigit) ) &&
+        lower.length > 2 &&
+        ( ( ! lower.equals ("was")) && ( ! lower.equals ("for")) )
+      }
+
+      val C = List.concat (vocab.C, getDistinctAlpha (AC.map { e => e._2 })).to[ListBuffer]
+
+      Array( A, B.to[ListBuffer], C ).map { v =>
+        v -= "was"
+        v -= "for"
+      }
+
+      VocabFactory.writeJSON (new Vocabulary (
+        A.toList,
+        B.toList,
+        C.toList,
+        true))
+       */
+    }
+    vocab
+    /*
+    val a = vocab.A.to[ListBuffer]
+    a -= "was"
+    a -= "for"
+
+    val b = vocab.B.to[ListBuffer]
+    b -= "was"
+    b -= "for"
+
+    val c = vocab.C.to[ListBuffer]
+    c -= "was"
+    c -= "for"
+
+    new Vocabulary (
+      a.toList,
+      b.toList,
+      c.toList
+    )
+ */
+  }
+
+  def getFlatBinaries (binaries : RDD[List[List[WordPair]]]) 
+      : RDD[WordPair] =
+  {
+    logger.info ("== Label known binaries based on CTD binaries...")
+    binaries.flatMap { item =>
+      item
+    }.flatMap { item =>
+      item
+    }.cache ()
+  }
+
+  def genHypothesesWithLRM (
+    binaries : RDD[List[List[WordPair]]],
+    AB       : RDD[(String, String, Int)],
+    BC       : RDD[(String, String, Int)]
+  ) = {
+
+    val fb = getFlatBinaries (binaries).map { item =>
+      ( formKey (item._1, item._2), ( item._3, item._4, item._5, item._6 ))
+    }.cache ()
+
+    /*
+     *  Get AB and BC pairs from CTD and associate with feature vectors
+     */
+    val abBinaries = joinBinaries (AB, fb)
+    val bcBinaries = joinBinaries (BC, fb)
+
+    val abLRM = trainLRM (abBinaries.union(bcBinaries))
+    val bcLRM = trainLRM (bcBinaries)
+
+    val abProb = modelProbabilities (abBinaries, abLRM)
+    val bcProb = modelProbabilities (bcBinaries, bcLRM)
+
+    logger.info ("== Hypotheses:")
+    val join = abProb.map { i =>
+      (i._2, (i._1, i._3, 1 ))
+    }.cogroup (
+      bcProb.map { j =>
+        (j._1, (j._2, j._3, 2 ))
+      }
+    ).cache ()
+
+    join.foreach { t =>
+      logger.info (s"join > $t")
+    }
+
+    join.flatMap { m =>
+      m._2._1.flatMap { n =>
+        m._2._2.map { p =>
+          (n._1, m._1, p._1, n._2 * p._2 )
+        }
+      }
+    }.cache ().foreach { triple =>
+      logger.info (s"hypothesis => $triple")
+    }
+
+  }
+
+  def calculateTriples (binaries : RDD[List[List[WordPair]]]) = {
+    logger.info ("== Calculate triples.")
+    val triples = binaries.flatMap { item =>
+      Processor.findTriples (item)
+    }.map { triple =>
+      ( triple, 1 )
+    }.reduceByKey (_ + _).map { tuple =>
+      ( tuple._1._1, tuple._1._2, tuple._1._3, tuple._2 )
+    }.distinct().sortBy (elem => -elem._4).cache ()
+
+    triples.foreach { a =>
+      logger.info (s"triple :-- ${a}")
+    }
+  }
+
+  /*
+   * predicted and ctd
+   * predicted and not ctd
+   * true negatives 
+   * 
+   * not predicted and ctd (false neg)
+   * not predicted and not ctd (true negatives)
+   * 
+   * precision and accuracy.
+   * 
+   */
+  def evaluateBinaries (
+    binaries : RDD[List[List[WordPair]]],
+    AB       : RDD[(String, String, Int)],
+    BC       : RDD[(String, String, Int)],
+    AC       : RDD[(String, String, Int)]
+  ) = {
+
+    logger.info ("Evaluate binaries")
+
+    val fb = getFlatBinaries (binaries).map { item =>
+      ( formKey (item._1, item._2), ( item._3, item._4, item._5, item._6 ))
+    }.cache ()
+
+    
+    logger.info (" -- joining binaries")
+    val abBinaries = joinBinaries (AB, fb)
+    val bcBinaries = joinBinaries (BC, fb)
+
+    logger.info (" -- counting predicted")
+    val abPredicted = fb.filter { e => e._2._4 == 1 }
+    val bcPredicted = fb.filter { e => e._2._4 == 2 }
+    val abPredictedCount = abPredicted.count ()
+    val bcPredictedCount = bcPredicted.count ()
+
+    logger.info (" -- counting ab binaries")
+    val abPredictedInCTDCount = abBinaries.count ()
+    logger.info (" -- counting bc binaries")
+    val bcPredictedInCTDCount = bcBinaries.count ()
+
+    bcPredicted.collect().foreach { x =>
+      logger.info (s"bc binary predicted: $x")
+    }
+    abPredicted.collect().foreach { x =>
+      logger.info (s"ab binary predicted: $x")
+    }
+
+    bcBinaries.collect().foreach { x =>
+      logger.info (s"bc binary in ctd: $x")
+    }
+    abBinaries.collect().foreach { x =>
+      logger.info (s"ab binary in ctd: $x")
+    }
+
+    println (s" ab predicted count:       $abPredictedCount")
+    println (s" bc predicted count:       $bcPredictedCount")
+    println (s" ab predicted and in ctd:  ${abBinaries.count ()}")
+    println (s" bc predicted and in ctd:  ${bcBinaries.count ()}")
+
+    val predictedAndNotInCTD = abBinaries
+    println (s"AB: predicted and in CTD: ")
+  }
+
+  def executeChemotextPipeline (
+    articlePaths : RDD[String],
+    meshXML      : String,
+    AB           : RDD[(String, String, Int)],
+    BC           : RDD[(String, String, Int)],
+    AC           : RDD[(String, String, Int)],
+    sampleSize   : Double) =
+  {
+
+    val vocab = extendVocabulary (AB, BC, AC, meshXML)
+
+    val binaries = generatePairs (articlePaths, meshXML, sampleSize)
+
+    evaluateBinaries (binaries, AB, BC, AC)
+
+    calculateTriples (binaries)
+
+    //genHypothesesWithLRM (binaries, AB, BC)
   }
 
 }
@@ -462,12 +631,25 @@ class PipelineContext (
     }
      */
 
+
+    val AB = Processor.getTSVRecords (sparkContext.textFile (ctdABPath)).map { row =>
+      ( row(0), row(3), 1 )
+    }.sample (false, 0.01, 1234)
+
+    val BC = Processor.getTSVRecords (sparkContext.textFile (ctdBCPath)).map { row =>
+      ( row(0), row(2), 2 )
+    }.sample (false, 0.01, 1234)
+
+    val AC = Processor.getTSVRecords (sparkContext.textFile (ctdACPath)).map { row =>
+      ( row(0), row(3), 3 )
+    }.sample (false, 0.01, 1234)
+
     Processor.executeChemotextPipeline (
       articlePaths = sparkContext.parallelize (articleList),
       meshXML      = meshXML,
-      ctdAC        = Processor.getTSVRecords (sparkContext.textFile (ctdACPath)),
-      ctdAB        = Processor.getTSVRecords (sparkContext.textFile (ctdABPath)),
-      ctdBC        = Processor.getTSVRecords (sparkContext.textFile (ctdBCPath)),
+      AB           = AB,
+      BC           = BC,
+      AC           = AC,
       sampleSize   = sampleSize.toDouble)
   }
 
