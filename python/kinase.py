ffa2f47256ba9372bec1089b8d7a6e3954d2e280
Steven Cox <scox@renci.org>
2016-06-07 18:04:43 -0400

    kinases

--- python/kinase.py
+++ python/kinase.py
@@ -22,230 +22,233 @@ from pyspark.sql import SQLContext
 
 logger = LoggingUtil.init_logging (__file__)
 
-def parse_inact_pmid (field):
-    logger = LoggingUtil.init_logging (__file__)
-    ''' imex:IM-24512|pubmed:26638075 '''
-    result = None
-    ids = field.split ("|")
-    for i in ids:
-        kv = i.split (":")
-        for k in kv:
-            if kv[0] == "pubmed":
-                result = kv[1]
+class InAct (object):
+    ''' Tools for managing the inAct data base export '''
+    @staticmethod
+    def parse_pmid (field):
+        logger = LoggingUtil.init_logging (__file__)
+        result = None
+        ids = field.split ("|")
+        for i in ids:
+            kv = i.split (":")
+            for k in kv:
+                if kv[0] == "pubmed":
+                    result = kv[1]
+                    break
+            if result:
                 break
-        if result:
-            break
-    return result
+        return result
+    @staticmethod
+    def parse_synonyms (synonyms, separator, result):
+        logger = LoggingUtil.init_logging (__file__)
+        synonym_pat = re.compile (r".*:([\w ]+)\(.*\)$", re.IGNORECASE)
+        synonyms = synonyms.split (separator)
+        for opt in synonyms:
+            match = synonym_pat.search (opt)
+            if match:
+                text = match.group(1).lower ()
+                result.append (text)
+        return result
+    @staticmethod
+    def map_As (inter, target):
+        result = []
+        syns = InAct.get_As (inter, target)
+        for syn in syns:
+            result.append ( ( syn, inter ) )
+        return result
+    @staticmethod
+    def get_As (inter, target):
+        result = []
+        synonyms = inter.alt_B if inter.A == target else inter.alt_A
+        return InAct.parse_synonyms (synonyms, "|", result)
+    @staticmethod
+    def get_Bs (inter, target):
+        result = []
+        synonyms = inter.alt_B if inter.B == target else inter.alt_A
+        return InAct.parse_synonyms (synonyms, "|", result)
 
-def get_article_dirs (articles, app_home):
-    logger.info ("Listing directory: {0}".format (articles))
-    cache = Cache (app_home)
-    dirs = cache.get ('pubmed_dirs.json')
-    c = 0
-    if dirs is None:
-        dirs = []
-        for root, dirnames, files in os.walk (articles):
-            for d in dirnames:
-                dirs.append (os.path.join (root, d))
-        cache.put ('pubmed_dirs.json', dirs)
-    return dirs
+class Ctext(object):
+    ''' Chemotext core logic '''
+    @staticmethod
+    def metalexer (article, A, B):
+        As = Ctext.lexer (article, A)
+        Bs = Ctext.lexer (article, B)
+        return Ctext.make_binaries (article, As, Bs)
+    @staticmethod
+    def lexer (article, terms):
+        logger = LoggingUtil.init_logging (__file__)
+        logger.debug ("Parsing @ {0}.json".format (article.fileName))
+        result = []
+        doc_pos = 0
+        para_pos = 0
+        sent_pos = 0
+        for para in article.paragraphs:
+            for sentence in para['sentences']:
+                sentence = sentence.replace (".", " ")
+                for term in terms.value:
+                    if not term or len(term) < 3:
+                        continue
+                    pos = sentence.find (term) if " " in term else sentence.find (" %s " % term)
+                    if pos > -1:
+                        result.append (WordPosition (word    = term,
+                                                     docPos  = doc_pos + pos,
+                                                     paraPos = para_pos,
+                                                     sentPos = sent_pos))
+                sent_pos = sent_pos + 1
+                doc_pos = doc_pos + len (sentence)
+            para_pos = para_pos + 1
+        for r in result:
+            logger.info ("word: {0} file: {1}".format (r, article.fileName))
+        return result
+    @staticmethod
+    def make_binaries (article, L, R, threshold=8000):
+        logger = LoggingUtil.init_logging (__file__)
+        result = []
+        for l in L:
+            for r in R:
+                distance = abs(l.docPos - r.docPos)
+                logger.debug ("Distance - {0}".format (distance))
+                if distance < threshold:
+                    if article.date.find ("--") > -1:
+                        article.date = "0-0-9000"
+                    binary = KinaseBinary (id = 0,
+                                           L = l.word,
+                                           R = r.word,
+                                           docDist  = abs(l.docPos - r.docPos),
+                                           paraDist = abs(l.paraPos - r.paraPos),
+                                           sentDist = abs(l.sentPos - r.sentPos),
+                                           code = 0,
+                                           fact = False,
+                                           refs = [],
+                                           pmid = article.id,
+                                           date = SUtil.parse_date (article.date),
+                                           file_name = article.fileName)
+                    logger.info ("Binary: {0}".format (binary))
+                    result.append (binary)
+        return result
+    @staticmethod
+    def map_date (r):
+        day = 0
+        month = 0
+        year = 0
+        pmid = r["PMID"]["#VALUE"]
+        date = r ["DateCreated"]
+        if date:
+            day = date["Day"]
+            month = date["Month"]
+            year = date["Year"]
+        return ( pmid, SUtil.parse_date ("{0}-{1}-{2}".format (day, month, year)) )
 
-def parse_inact_synonyms (synonyms, separator, result):
-    logger = LoggingUtil.init_logging (__file__)
-    synonym_pat = re.compile (r".*:([\w ]+)\(.*\)$", re.IGNORECASE)
-    synonyms = synonyms.split (separator)
-    for opt in synonyms:
-        match = synonym_pat.search (opt)
-        if match:
-            text = match.group(1).lower ()
-            result.append (text)
-    return result
-
-def map_As (inter, target):
-    result = []
-    syns = get_As (inter, target)
-    for syn in syns:
-        result.append ( ( syn, inter ) )
-    return result
-
-def get_As (inter, target):
-    result = []
-    synonyms = inter.alt_B if inter.A == target else inter.alt_A
-    return parse_inact_synonyms (synonyms, "|", result)
-
-def get_Bs (inter, target):
-    result = []
-    synonyms = inter.alt_B if inter.B == target else inter.alt_A
-    return parse_inact_synonyms (synonyms, "|", result)
-
-def metalexer (article, A, B):
-    As = lexer (article, A)
-    Bs = lexer (article, B)
-    return make_binaries (article, As, Bs)
-
-def lexer (article, terms):
-    logger = LoggingUtil.init_logging (__file__)
-    logger.debug ("Parsing @ {0}.json".format (article.fileName))
-    result = []
-    doc_pos = 0
-    para_pos = 0
-    sent_pos = 0
-    for para in article.paragraphs:
-        for sentence in para['sentences']:
-            sentence = sentence.replace (".", " ")
-            for term in terms.value:
-                if not term or len(term) < 3:
-                    continue
-                pos = sentence.find (term) if " " in term else sentence.find (" %s " % term)
-                if pos > -1:
-                    result.append (WordPosition (word    = term,
-                                                 docPos  = doc_pos + pos,
-                                                 paraPos = para_pos,
-                                                 sentPos = sent_pos))
-            sent_pos = sent_pos + 1
-            doc_pos = doc_pos + len (sentence)
-        para_pos = para_pos + 1
-    for r in result:
-        logger.info ("word: {0} file: {1}".format (r, article.fileName))
-    return result
-
-def make_binaries (article, L, R, threshold=8000):
-    logger = LoggingUtil.init_logging (__file__)
-    result = []
-    for l in L:
-        for r in R:
-            distance = abs(l.docPos - r.docPos)
-            logger.debug ("Distance - {0}".format (distance))
-            if distance < threshold:
-                if article.date.find ("--") > -1:
-                    article.date = "0-0-9000"
-                binary = KinaseBinary (id = 0,
-                                       L = l.word,
-                                       R = r.word,
-                                       docDist  = abs(l.docPos - r.docPos),
-                                       paraDist = abs(l.paraPos - r.paraPos),
-                                       sentDist = abs(l.sentPos - r.sentPos),
-                                       code = 0,
-                                       fact = False,
-                                       refs = [],
-                                       pmid = article.id,
-                                       date = SUtil.parse_date (article.date),
-                                       file_name = article.fileName)
-                logger.info ("Binary: {0}".format (binary))
-                result.append (binary)
-    return result
-
-def map_date (r):
-    day = 0
-    month = 0
-    year = 0
-    pmid = r["PMID"]["#VALUE"]
-    date = r ["DateCreated"]
-    if date:
-        day = date["Day"]
-        month = date["Month"]
-        year = date["Year"]
-    return ( pmid, SUtil.parse_date ("{0}-{1}-{2}".format (day, month, year)) )
-
-def load_inact (sc, conf):
-    logger.info ("Load inact db...")
-    sqlContext = SQLContext(sc)
-    return sqlContext.read.                     \
+class DataLake(object):
+    ''' Tools for connecting to data sources '''
+    @staticmethod
+    def load_articles (sc, conf):
+        logger.info ("Load PubMed Central preprocessed to JSON as an RDD of article objects")
+        articles = glob.glob (os.path.join (conf.input_dir, "*fxml.json"))
+        return sc.parallelize (articles). \
+            map (lambda a : SUtil.read_article (a)). \
+            cache ()
+    @staticmethod
+    def load_inact (sc, conf):
+        logger.info ("Load inact db...")
+        sqlContext = SQLContext(sc)
+        return sqlContext.read.                     \
             format('com.databricks.spark.csv'). \
             options(comment='#',                \
                     delimiter='\t').            \
             load(conf.inact).rdd.               \
             map (lambda r : P53Inter ( A = r.C0, B = r.C1, 
                                        alt_A = r.C4, alt_B = r.C5, 
-                                       pmid = parse_inact_pmid(r.C8) ) ) . \
+                                       pmid = InAct.parse_pmid(r.C8) ) ) . \
             cache ()
-
-def load_pro_qinase (sc, conf):
-    sqlContext = SQLContext(sc)
-    return sqlContext.read. \
-        format('com.databricks.spark.csv'). \
-        options (delimiter=' '). \
-        load (conf.kinase_synonyms).rdd. \
-        flatMap (lambda r : ProQinaseSynonyms (r.C1, r.C2, r.C3).get_names ()). \
-        cache ()
-
-def load_pmid_date (sc, conf):
-    logger.info ("Load medline data to determine dates by pubmed ids")
-    sqlContext = SQLContext (sc)
-    pmid_date = sqlContext.read.format ('com.databricks.spark.xml'). \
+    @staticmethod
+    def load_pro_qinase (sc, conf):
+        logger.info ("Load ProQinase Kinase synonym db...")
+        sqlContext = SQLContext(sc)
+        return sqlContext.read. \
+            format('com.databricks.spark.csv'). \
+            options (delimiter=' '). \
+            load (conf.kinase_synonyms).rdd. \
+            flatMap (lambda r : ProQinaseSynonyms (r.C1, r.C2, r.C3).get_names ()). \
+            cache ()
+    @staticmethod
+    def load_pmid_date (sc, conf):
+        logger.info ("Load medline data to determine dates by pubmed ids")
+        sqlContext = SQLContext (sc)
+        pmid_date = sqlContext.read.format ('com.databricks.spark.xml'). \
         options(rowTag='MedlineCitation').load(conf.medline)
-    #sample (False, 0.02)
-    return pmid_date. \
-        select (pmid_date["DateCreated"], pmid_date["PMID"]). \
-        rdd. \
-        map (lambda r : map_date (r))
+        #sample (False, 0.02)
+        return pmid_date. \
+            select (pmid_date["DateCreated"], pmid_date["PMID"]). \
+            rdd. \
+            map (lambda r : Ctext.map_date (r))
+    @staticmethod
+    def extend_A (sc, A, pro_qinase, mesh):
+        A = A.filter (lambda r : r.find ("kinase") > -1).distinct ()
+        logger.info ("Kinases from inAct: {0}".format (A.count ()))
 
-def extend_A (sc, A, pro_qinase, mesh):
-    A = A.filter (lambda r : r.find ("kinase") > -1).distinct ()
-    logger.info ("Kinases from inAct: {0}".format (A.count ()))
+        A = A.union (pro_qinase)
+        logger.info ("Kinases from inAct+ProQinase: {0}".format (A.count ()))
 
-    A = A.union (pro_qinase)
-    logger.info ("Kinases from inAct+ProQinase: {0}".format (A.count ()))
+        logger.info ("Add MeSH derived kinase terms to list of As...")
+        skiplist = [ 'for', 'gene', 'complete', None ]
+        with open ("kinases.json", "r") as stream:
+            mesh = sc.parallelize (json.loads (stream.read ()))
+            A = A.union (mesh). \
+                filter (lambda a : a not in skiplist). \
+                distinct ()
+            # TEST
+            A = A.union (sc.parallelize ([ "protein kinase c inhibitor protein 1" ]) )
+            logger.info ("Total kinases from inAct/MeSH: {0}".format (A.count ()))
+        return A
+    @staticmethod
+    def load_vocabulary (sc, conf):
+        logger.info ("Build A / B vocabulary from inact/pro_qinase/mesh...")
+        inact = DataLake.load_inact (sc, conf)
+        A = inact.flatMap (lambda inter : InAct.get_As (inter, 'uniprotkb:P04637')).distinct().cache ()
+        B = inact.flatMap (lambda inter : InAct.get_Bs (inter, 'uniprotkb:P04637')).distinct().cache ()
+        pro_qinase = DataLake.load_pro_qinase (sc, conf)
+        A = DataLake.extend_A (sc, A, pro_qinase, "kinases.json")
+        return Vocabulary (A, B, ref=inact)
 
-    logger.info ("Add MeSH derived kinase terms to list of As...")
-    skiplist = [ 'for', 'gene', 'complete', None ]
-    with open ("kinases.json", "r") as stream:
-        mesh = sc.parallelize (json.loads (stream.read ()))
-        A = A.union (mesh). \
-            filter (lambda a : a not in skiplist). \
-            distinct ()
-        # TEST
-        A = A.union (sc.parallelize ([ "protein kinase c inhibitor protein 1" ]) )
-        logger.info ("Total kinases from inAct/MeSH: {0}".format (A.count ()))
-    return A
-
-def load_vocabulary (sc, conf):
-    logger.info ("Build A / B vocabulary from inact/pro_qinase/mesh...")
-    inact = load_inact (sc, conf)
-    A = inact.flatMap (lambda inter : get_As (inter, 'uniprotkb:P04637')).distinct().cache ()
-    B = inact.flatMap (lambda inter : get_Bs (inter, 'uniprotkb:P04637')).distinct().cache ()
-    pro_qinase = load_pro_qinase (sc, conf)
-    A = extend_A (sc, A, pro_qinase, "kinases.json")
-    return Vocabulary (A, B, ref=inact)
-
-def find_interactions (sc, vocabulary, articles):
-    logger.info ("Find kinase-p53 interactions in article text.")
-    broadcast_A = sc.broadcast (vocabulary.A.collect ())
-    broadcast_B = sc.broadcast (vocabulary.B.collect ())
-    return articles.flatMap (lambda a : metalexer (a, broadcast_A, broadcast_B) ).cache ()
-
-def find_facts (vocabulary, binaries):
-    logger.info ("Join matches from full text with the inact database to find 'facts'.")
-    binaries_map = binaries.map (lambda r : ( r.L, r) ) # ( A -> KinaseBinary.L )
-    inact_map = vocabulary.ref.flatMap (lambda inter : map_As(inter, 'uniprotkb:P04637') ) # ( A -> P53Inter )
-    return binaries_map.join (inact_map) # ( A -> ( KinaseBinary, P53Inter ) )
-    
-def find_before (pmid_date, facts):
-    logger.info ("Join facts with the pmid->date map to find interactions noticed before published discovery.")
-    ref_pmid_to_binary = facts.map (lambda r : ( r[1][1].pmid, r[1][0] ) ) # ( inAct.REF[pmid] -> KinaseBinary )
-    # TEST. Add reference pmids with late dates.
-    pmid_date = pmid_date.union (ref_pmid_to_binary.map (lambda r : ( r[0], SUtil.parse_date ("1-1-2300") )))
-    before = ref_pmid_to_binary.                             \
-        join (pmid_date).                                    \
-        map (lambda r : r[1][0].copy (ref_date = r[1][1]) ). \
-        filter (lambda k : k.date < k.ref_date).             \
-        distinct ()
-    return before
-
-def load_articles (sc, conf):
-    logger.info ("Load preprocessed JSON as an RDD of article objects")
-    return sc.parallelize (glob.glob (os.path.join (conf.input_dir, "*fxml.json"))). \
-               map (lambda a : SUtil.read_article (a)). \
-               cache ()
+class LitCrawl(object):
+    ''' Crawl the literature in search of interactions '''
+    @staticmethod
+    def find_interactions (sc, vocabulary, articles):
+        logger.info ("Find kinase-p53 interactions in article text.")
+        broadcast_A = sc.broadcast (vocabulary.A.collect ())
+        broadcast_B = sc.broadcast (vocabulary.B.collect ())
+        return articles.flatMap (lambda a : Ctext.metalexer (a, broadcast_A, broadcast_B) ).cache ()
+    @staticmethod
+    def find_facts (vocabulary, binaries):
+        logger.info ("Join matches from full text with the inact database to find 'facts'.")
+        binaries_map = binaries.map (lambda r : ( r.L, r) ) # ( A -> KinaseBinary.L )
+        inact_map = vocabulary.ref.flatMap (lambda inter : InAct.map_As(inter, 'uniprotkb:P04637') ) # ( A -> P53Inter )
+        return binaries_map.join (inact_map) # ( A -> ( KinaseBinary, P53Inter ) )    
+    @staticmethod
+    def find_before (pmid_date, facts):
+        logger.info ("Join facts with the pmid->date map to find interactions noticed before published discovery.")
+        ref_pmid_to_binary = facts.map (lambda r : ( r[1][1].pmid, r[1][0] ) ) # ( inAct.REF[pmid] -> KinaseBinary )
+        # TEST. Add reference pmids with late dates.
+        pmid_date = pmid_date.union (ref_pmid_to_binary.map (lambda r : ( r[0], SUtil.parse_date ("1-1-2300") )))
+        before = ref_pmid_to_binary.                             \
+                 join (pmid_date).                                    \
+                 map (lambda r : r[1][0].copy (ref_date = r[1][1]) ). \
+                 filter (lambda k : k.date < k.ref_date).             \
+                 distinct ()
+        return before
 
 def execute (conf, home):
     sc = SparkUtil.get_spark_context (conf)
-    articles = load_articles (sc, conf)
-    vocabulary = load_vocabulary (sc, conf)
-    binaries = find_interactions (sc, vocabulary, articles)
-    facts = find_facts (vocabulary, binaries)
-    pmid_date = load_pmid_date (sc, conf) # ( pmid -> date )
-    before = find_before (pmid_date, facts)
+
+    articles = DataLake.load_articles (sc, conf)
+    vocabulary = DataLake.load_vocabulary (sc, conf)
+    pmid_date = DataLake.load_pmid_date (sc, conf) # ( pmid -> date )
+
+    binaries = LitCrawl.find_interactions (sc, vocabulary, articles)
+    facts = LitCrawl.find_facts (vocabulary, binaries)
+    before = LitCrawl.find_before (pmid_date, facts)
+
     for m in before.collect ():
         logger.info ("Before-Ref-Date:> {0}".format (m))
 
