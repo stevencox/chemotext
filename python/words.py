fdb51cf58c3871107b9abbcf6797257227829075
StarsCluster <scox@renci.org>
2016-10-20 13:30:30 -0400

    gensim w2v

--- python/words.py
+++ python/words.py
@@ -55,24 +55,23 @@ def build (conf):
 
 def generate_year_model (w2v_dir, year, sentences):
     model = None
-#    print ("sentences length in w2v> {0}".format (len(sentences)))
-    if len(sentences) > 0:
-        model = gensim.models.Word2Vec (sentences, workers=8)
+    count = len (sentences)
+    if count > 0:
+        workers = 16 if count > 1000 else 4
+        model = gensim.models.Word2Vec (sentences, workers=workers)
         file_name = os.path.join (w2v_dir, "pmc-{0}.w2v".format (year))
-        print ("  *** Writing model file => {0}".format (file_name))
+        print ("  ==== *** ==== Writing model file => {0}".format (file_name))
         model.save (file_name)
     return model
 
 def sentences_by_year (a):
-    result = ( 0, [] )
+    result = ( '0', [] )
     if a is not None and a.date is not None and a.date.count ('-') == 2:
-        print ("--------------> {0}".format (a.date))
         year = a.date.split('-')[2]
         if len(year) >= 4:
-            result = (
-                year[:4],
-                [ s.split(' ') for p in a.paragraphs for s in p.sentences ]
-            )
+            year = year[:4]
+            words = [ s.split(' ') for p in a.paragraphs for s in p.sentences ]
+            result = ( year, words )
     return result
 
 def build_models (conf):
@@ -109,7 +108,45 @@ def main ():
  
 main ()
 
+'''
+https://github.com/joblib/joblib/issues/122
 
+
+16/10/20 11:37:59 ERROR Executor: Exception in task 0.0 in stage 1.2 (TID 359)
+org.apache.spark.api.python.PythonException: Traceback (most recent call last):
+  File "/scratch/mesos/slaves/20160711-145321-1946687916-5050-30249-S132114/frameworks/20160711-145321-1946687916-5050-30249-0518/executors/3/runs/4e72994f-4b97-4bdc-97e4-28163646eb6e/spark-1.6.0-bin-hadoop2.6/python/lib/pyspark.zip/pyspark/worker.py", line 111, in main
+    process()
+  File "/scratch/mesos/slaves/20160711-145321-1946687916-5050-30249-S132114/frameworks/20160711-145321-1946687916-5050-30249-0518/executors/3/runs/4e72994f-4b97-4bdc-97e4-28163646eb6e/spark-1.6.0-bin-hadoop2.6/python/lib/pyspark.zip/pyspark/worker.py", line 106, in process
+    serializer.dump_stream(func(split_index, iterator), outfile)
+  File "/projects/stars/stack/spark/current/python/lib/pyspark.zip/pyspark/rdd.py", line 2346, in pipeline_func
+  File "/projects/stars/stack/spark/current/python/lib/pyspark.zip/pyspark/rdd.py", line 2346, in pipeline_func
+  File "/projects/stars/stack/spark/current/python/lib/pyspark.zip/pyspark/rdd.py", line 2346, in pipeline_func
+  File "/projects/stars/stack/spark/current/python/lib/pyspark.zip/pyspark/rdd.py", line 2346, in pipeline_func
+  File "/projects/stars/stack/spark/current/python/lib/pyspark.zip/pyspark/rdd.py", line 317, in func
+  File "/projects/stars/stack/spark/current/python/lib/pyspark.zip/pyspark/rdd.py", line 1784, in _mergeCombiners
+  File "/scratch/mesos/slaves/20160711-145321-1946687916-5050-30249-S132114/frameworks/20160711-145321-1946687916-5050-30249-0518/executors/3/runs/4e72994f-4b97-4bdc-97e4-28163646eb6e/spark-1.6.0-bin-hadoop2.6/python/lib/pyspark.zip/pyspark/shuffle.py", line 287, in mergeCombiners
+    self._spill()
+  File "/scratch/mesos/slaves/20160711-145321-1946687916-5050-30249-S132114/frameworks/20160711-145321-1946687916-5050-30249-0518/executors/3/runs/4e72994f-4b97-4bdc-97e4-28163646eb6e/spark-1.6.0-bin-hadoop2.6/python/lib/pyspark.zip/pyspark/shuffle.py", line 315, in _spill
+    self.serializer.dump_stream([(k, v)], streams[h])
+  File "/scratch/mesos/slaves/20160711-145321-1946687916-5050-30249-S132114/frameworks/20160711-145321-1946687916-5050-30249-0518/executors/3/runs/4e72994f-4b97-4bdc-97e4-28163646eb6e/spark-1.6.0-bin-hadoop2.6/python/lib/pyspark.zip/pyspark/serializers.py", line 267, in dump_stream
+    bytes = self.serializer.dumps(vs)
+  File "/scratch/mesos/slaves/20160711-145321-1946687916-5050-30249-S132114/frameworks/20160711-145321-1946687916-5050-30249-0518/executors/3/runs/4e72994f-4b97-4bdc-97e4-28163646eb6e/spark-1.6.0-bin-hadoop2.6/python/lib/pyspark.zip/pyspark/serializers.py", line 487, in dumps
+    return zlib.compress(self.serializer.dumps(obj), 1)
+OverflowError: size does not fit in an int
+
+at org.apache.spark.api.python.PythonRunner$$anon$1.read(PythonRDD.scala:166)
+at org.apache.spark.api.python.PythonRunner$$anon$1.<init>(PythonRDD.scala:207)
+at org.apache.spark.api.python.PythonRunner.compute(PythonRDD.scala:125)
+at org.apache.spark.api.python.PythonRDD.compute(PythonRDD.scala:70)
+at org.apache.spark.rdd.RDD.computeOrReadCheckpoint(RDD.scala:306)
+at org.apache.spark.rdd.RDD.iterator(RDD.scala:270)
+at org.apache.spark.scheduler.ResultTask.runTask(ResultTask.scala:66)
+at org.apache.spark.scheduler.Task.run(Task.scala:89)
+at org.apache.spark.executor.Executor$TaskRunner.run(Executor.scala:213)
+at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1145)
+at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:615)
+at java.lang.Thread.run(Thread.java:745)
+'''
 
 #    with open ("out.txt", "w") as stream:
 #        stream.write ("sentence_by_year keys: {0}".format (sent_by_year.map(lambda sy : sy[0]).collect ()))
